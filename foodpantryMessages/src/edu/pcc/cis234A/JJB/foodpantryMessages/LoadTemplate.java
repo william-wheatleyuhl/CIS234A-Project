84b70449472eb3f16e6ff44f627a67fde9944af4
shipwreckwill <shipwreckwill@gmail.com>
2019-04-22 22:49:15 -0700

    Begin DB Integration; Adjusted GUI layout

--- foodpantryMessages/src/edu/pcc/cis234A/JJB/foodpantryMessages/LoadTemplate.java
+++ foodpantryMessages/src/edu/pcc/cis234A/JJB/foodpantryMessages/LoadTemplate.java
@@ -1,5 +1,8 @@
 package edu.pcc.cis234A.JJB.foodpantryMessages;
 
+/*
+    This Class will parse message templates and fill in applicable tags
+ */
 public class LoadTemplate {
-    
+
 }
--- foodpantryMessages/src/edu/pcc/cis234A/JJB/foodpantryMessages/NotificationForm.java
+++ foodpantryMessages/src/edu/pcc/cis234A/JJB/foodpantryMessages/NotificationForm.java
@@ -4,18 +4,14 @@ import javax.swing.*;
 public class NotificationForm {
     private JPanel rootPanel;
     private JLabel userLoggedInLabel;
-    private JLabel templateLabel;
-    private JRadioButton templateRadio1;
-    private JRadioButton templateRadio2;
-    private JRadioButton templateRadio3;
-    private JRadioButton noneRadio;
-    private JLabel sendToLabel;
-    private JRadioButton allRadio;
-    private JRadioButton specificRadio;
-    private JTextField specificField;
     private JTextArea notificationTextArea;
     private JLabel editTextLabel;
     private JButton sendNotificationButton;
+    private JLabel sendToLabel;
+    private JRadioButton allRadio;
+    private JRadioButton specificRadio;
+    private JTextField recipientField;
+    private JComboBox chooseTemplate;
 
     public JPanel getRootPanel() {
         return rootPanel;
