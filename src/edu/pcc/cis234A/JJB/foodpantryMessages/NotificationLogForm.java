555c0e9cce53d508277c6f5249320b7e1c15addb
ClimbingVine <32885549+ClimbingVine@users.noreply.github.com>
2019-04-22 21:43:13 -0700

    Added jar and date pickers
    Added calendar jar in lib, date pickers, and croll pane for full text message.

--- src/edu/pcc/cis234A/JJB/foodpantryMessages/NotificationLogForm.java
+++ src/edu/pcc/cis234A/JJB/foodpantryMessages/NotificationLogForm.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis234A.JJB.foodpantryMessages;
 
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,16 +13,28 @@ public class NotificationLogForm {
     private JPanel rootPanel;
     private JLabel pageName;
     private JLabel userLoggedInLabel;
-    private JLabel dateLabel;
-    private JLabel timeLabel;
+    private JLabel messageNumberLabel;
+    private JLabel timestampLabel;
     private JLabel sentByLabel;
-    private JLabel sentToLabel;
-    private JLabel messageLabel;
+    private JLabel recipientCountLabel;
+    private JLabel messagePreviewLabel;
     private JTable notificationDataTable;
     private JScrollPane tableScrollPane;
+    private JLabel minDateLabel;
+    private JPanel minDatePanel;
+    private JLabel maxDateLabel;
+    private JPanel maxDatePanel;
+    private JLabel fullMessageLabel;
+    private JScrollPane fullMessageScrollPane;
+
+    JDateChooser jMinDateChooser = new JDateChooser();
+    JDateChooser jMaxDateChooser = new JDateChooser();
 
     public NotificationLogForm() {
         rootPanel.setPreferredSize(new Dimension(800, 600));
+
+        minDatePanel.add(jMinDateChooser);
+        maxDatePanel.add(jMaxDateChooser);
     }
 
     public JPanel getRootPanel() {
