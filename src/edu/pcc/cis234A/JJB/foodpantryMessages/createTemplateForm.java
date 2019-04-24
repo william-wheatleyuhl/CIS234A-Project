606731d7f2c0d2aca40afbdef7cee9b24b14ab0d
Syn Calvo <syn@syncalvo.com>
2019-04-24 00:11:30 -0700

    Added action when Save button clicked

--- src/edu/pcc/cis234A/JJB/foodpantryMessages/createTemplateForm.java
+++ src/edu/pcc/cis234A/JJB/foodpantryMessages/createTemplateForm.java
@@ -2,6 +2,8 @@ package edu.pcc.cis234A.JJB.foodpantryMessages;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author Syn
@@ -12,7 +14,7 @@ public class createTemplateForm {
     private JLabel labelPageName;
     private JLabel labelLoggedInAs;
     private JRadioButton radioCreateNew;
-    private JRadioButton RadioEditExisting;
+    private JRadioButton radioEditExisting;
     private JLabel labelTemplateName;
     private JTextField fieldTemplateName;
     private JLabel labelTemplateText;
@@ -23,8 +25,54 @@ public class createTemplateForm {
 
     public createTemplateForm() {
         rootPanel.setPreferredSize(new Dimension(800, 600));
+        buttonSave.addActionListener(new ActionListener() {
+            /**
+             * Action performed when "Save" button is clicked
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean checkTemplateName = false;
+                boolean checkTemplateText = false;
+
+                // Check that the Template Name Field has something in it
+                if(fieldTemplateName.getText().equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please enter a Template Name");
+                }
+                else {
+                    checkTemplateName = true;
+                }
+
+                // Check that the Template Text Area has something in it
+                if(areaTemplateText.getText().equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please enter text for the template");
+                }
+                else {
+                    checkTemplateText = true;
+                }
+
+                if (checkTemplateName && checkTemplateText) {
+                    if(radioCreateNew.isSelected()) {
+                        // Placeholder - In the future the save button will commit information to
+                        // DB, inform user it has been saved, and refresh the page
+                        JOptionPane.showMessageDialog(null, "New template saved");
+                    }
+                    else if(radioEditExisting.isSelected()) {
+                        // Placeholder - In the future the save button will commit information to
+                        // DB, inform user it has been saved, and refresh the page
+                        JOptionPane.showMessageDialog(null, "Changes to <template name> saved");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Choose \"Create New\" or select an existing template to save");
+                    }
+                }
+            }
+        });
     }
 
+    /**
+     * @return rootPanel the root panel for the GUI
+     */
     public JPanel getRootPanel() {
         return rootPanel;
     }
