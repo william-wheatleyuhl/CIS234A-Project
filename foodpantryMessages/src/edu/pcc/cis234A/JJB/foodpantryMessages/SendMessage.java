c1bccaa6036849637f4ca272a79a61014ad5b3a4
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2019-05-06 21:07:29 -0700

    SendMessage Now functional. Converting to be used to auto send messages to intended recipients.

--- foodpantryMessages/src/edu/pcc/cis234A/JJB/foodpantryMessages/SendMessage.java
+++ foodpantryMessages/src/edu/pcc/cis234A/JJB/foodpantryMessages/SendMessage.java
@@ -11,8 +11,8 @@ public class SendMessage {
     public static void main(String[] args) {
         String recipient = "william.wheatleyuhl@pcc.edu";
         String sentFrom = "shipwreckwill@gmail.com";
-        final String username= "shipwreckwill@gmail.com";
-        final String password = "pazrjknlqccqhklx";
+        final String username= "jjb.234a.test@gmail.com";
+        final String password = "vK!18%sPP5*nssJE";
         String host = "smtp.gmail.com";
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -21,9 +21,9 @@ public class SendMessage {
         props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.smtp.socketFactory.class",
                 "javax.net.ssl.SSLSocketFactory");
-        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+        Session session = Session.getDefaultInstance(props, new Authenticator() {
            protected PasswordAuthentication getPassWordAuthentication() {
-               return new PasswordAuthentication(username, password);
+               return new PasswordAuthentication("shipwreckwill@gmail.com", "pazrjknlqccqhklx");
            }
         });
 
@@ -33,12 +33,11 @@ public class SendMessage {
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
             message.setSubject("Test Message");
             message.setText("Test Message");
-            Transport transport = session.getTransport("smtp");
-            transport.connect(host, 587, username, password);
-            Transport.send(message);
+            Transport.send(message, username, password);
             System.out.println("Message Sent");
         } catch(MessagingException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+//            throw new RuntimeException(e);
         }
     }
 
