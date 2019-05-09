7e20e210c1e785243669dbce2288f209d13c0350
jeffh123 <jeffhumphrey16@hotmail.com>
2019-05-08 22:53:10 -0400

    minor bug fix.

--- src/edu/pcc/cis234A/JJB/foodpantryMessages/Presentation.java
+++ src/edu/pcc/cis234A/JJB/foodpantryMessages/Presentation.java
@@ -2,11 +2,13 @@ package edu.pcc.cis234A.JJB.foodpantryMessages;
 
 import javax.swing.*;
 
+
 /**
  * This class represents the implementation of the Presentation GUI. In this class, other modules are poulated
  * into a JTabbedPane, and can be selected by using the tabs at the top of the screen.
  * @author William Wheatley-Uhl
  * @version 2019.05.06
+ *
  */
 public class Presentation {
     UserLogin userLogin1 = new UserLogin();
