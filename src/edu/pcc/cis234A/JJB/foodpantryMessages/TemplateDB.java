398f5fdd338f248528f64f6e7e2c8a65ccb7448b
Syn Calvo <syn@syncalvo.com>
2019-06-04 23:22:04 -0700

    Added getLastEditUser() method

--- src/edu/pcc/cis234A/JJB/foodpantryMessages/TemplateDB.java
+++ src/edu/pcc/cis234A/JJB/foodpantryMessages/TemplateDB.java
@@ -1,11 +1,7 @@
 package edu.pcc.cis234A.JJB.foodpantryMessages;
 
-//import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
-
-import javax.xml.transform.Templates;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Connects to the 234a_JavaneseJumpingBeans database & returns requested data based on SQL queries
@@ -20,13 +16,37 @@ public class TemplateDB {
     private static final String USERNAME = "234a_JavaneseJumpingBeans";
     private static final String PASSWORD = "Nullifying9Defeating%";
     private static final String NAME_QUERY = "SELECT Username, LastName, FirstName FROM [USER]";
-    private static final String TEMPLATE_QUERY = "SELECT TemplateID, TemplateName, MessageText FROM TEMPLATE";
+    private static final String TEMPLATE_QUERY = "SELECT TemplateID, TemplateName, MessageText, UserID FROM TEMPLATE";
     private static final String ID_QUERY = "SELECT TemplateID FROM TEMPLATE";
     private static final String LOG_TEMPLATE = "INSERT INTO TEMPLATE VALUES(?,?,?,?)";
     private static final String UPDATE_TEMPLATE = "UPDATE TEMPLATE SET TemplateName = ?, MessageText = ?, UserID = ? WHERE TemplateID = ?";
+    private static final String LAST_EDIT = "SELECT * FROM [USER]";
 
     private static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+    }
+
+    /**
+     * Get username of the user who last edited a template
+     * @return lastEditUser The username based on UserID
+     */
+    //TODO: Fix this brokeness
+    public String getLastEditUser(int lastEditUserID) {
+        String lastEditUser = "";
+        try (
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = stmt.executeQuery(LAST_EDIT)
+        ) {
+            while(rs.next()) {
+                if(rs.getInt("UserID") == (lastEditUserID)){
+                    lastEditUser = rs.getString("Username");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lastEditUser;
     }
 
     /**
@@ -43,7 +63,8 @@ public class TemplateDB {
             while (rs.next()) {
                 templates.add(new Template(rs.getInt("TemplateID"),
                         rs.getString("TemplateName"),
-                        rs.getString("MessageText")));
+                        rs.getString("MessageText"),
+                        rs.getInt("UserID")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -74,18 +95,18 @@ public class TemplateDB {
 
     /**
      * Submit NEW template data to DB when "Save" button is clicked
-     * @param newTemplateString
-     * @param newTemplateName
+     * @param newTemplateString The template text to be added to DB
+     * @param newTemplateName The template name to be added to DB
+     * @param userID The user currently logged in
      */
-    public void logNewTemplate(String newTemplateName, String newTemplateString) {
+    public void logNewTemplate(String newTemplateName, String newTemplateString, int userID) {
         try {
             Connection conn = getConnection();
             PreparedStatement stmt = conn.prepareStatement(LOG_TEMPLATE);
             stmt.setInt(1, getLastTemplateID() + 1);
             stmt.setString(2, newTemplateName);
             stmt.setString(3, newTemplateString);
-            //TODO: Use UserID from session for below stmt
-            stmt.setInt(4, 1);
+            stmt.setInt(4, userID);
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -94,17 +115,18 @@ public class TemplateDB {
 
     /**
      * Update an existing template in DB when "Save" button is clicked
-     * @param existingTemplateString
-     * @param existingTemplateName
+     * @param existingTemplateString The template text to be changed / updated in DB
+     * @param existingTemplateName The template name to be changed / updated in DB
+     * @param userID The user currently logged in
+     * @param existingTemplateID The TemplateID from the DB identifying which row to modify
      */
-    public void updateExistingTemplate(String existingTemplateName, String existingTemplateString, int existingTemplateID) {
+    public void updateExistingTemplate(String existingTemplateName, String existingTemplateString, int userID, int existingTemplateID) {
         try {
             Connection conn = getConnection();
             PreparedStatement stmt = conn.prepareStatement(UPDATE_TEMPLATE);
             stmt.setString(1, existingTemplateName);
             stmt.setString(2, existingTemplateString);
-            //TODO: Use UserID from session for below stmt
-            stmt.setInt(3, 1);
+            stmt.setInt(3, userID);
             stmt.setInt(4, existingTemplateID);
             stmt.executeUpdate();
         } catch (SQLException e) {
