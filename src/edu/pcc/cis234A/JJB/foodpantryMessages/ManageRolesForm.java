67825ff6b2332711fd24e15b58b03177866d3e0c
Syn Calvo <syn@syncalvo.com>
2019-06-10 03:37:22 -0700

    new groups now save to DB, existing groups still need description populated

--- src/edu/pcc/cis234A/JJB/foodpantryMessages/ManageRolesForm.java
+++ src/edu/pcc/cis234A/JJB/foodpantryMessages/ManageRolesForm.java
@@ -20,9 +20,11 @@ import java.util.HashMap;
  *
  * Changelog:
  * 20190609 WWU - Implemented working roles class
- * 20190603 WWU - Populate "Current Role" label dynamically
- * 20190603 WWU - Finished Submit button for changing subscriber roles.
- * 20190603 WWU - comboBoxGroups now populates, as does the fieldGroupName textField
+ * 20190609 WWU - Populate "Current Role" label dynamically
+ * 20190609 WWU - Finished Submit button for changing subscriber roles.
+ * 20190609 WWU - comboBoxGroups now populates, as does the fieldGroupName textField
+ * 20190609 SC - Added functionality to save groups to DB
+ * 20190609 SC - Cleaned up completed TODOs & added new TODOs
  */
 public class ManageRolesForm {
     private JPanel rootPanel;
@@ -45,6 +47,8 @@ public class ManageRolesForm {
     private JLabel labelManageRoles;
     private JComboBox comboBoxGroups;
     private JScrollPane scrollPaneUsers;
+    private JLabel labelGroupDesc;
+    private JTextField fieldGroupDesc;
     private JList listUsers;
     SubscriberDB subs = new SubscriberDB();
     private ArrayList<Recipient> recipients = subs.readSubscriberData();
@@ -54,6 +58,8 @@ public class ManageRolesForm {
     private int roleID;
     private int userID;
     private String newGroupName;
+    private String newGroupDesc;
+    private int existingGroupID;
 
     DefaultComboBoxModel modelUsers = (DefaultComboBoxModel) comboBoxUsers.getModel();
     DefaultComboBoxModel modelRoles = (DefaultComboBoxModel) comboBoxRoles.getModel();
@@ -70,6 +76,7 @@ public class ManageRolesForm {
         comboBoxGroups.setEnabled(false);
         fieldGroupName.setEnabled(false);
         scrollPaneUsers.setEnabled(false);
+        listUsers.setEnabled(false);
         buttonSubmitRole.setEnabled(false);
 
         rootPanel.setPreferredSize(new Dimension(800, 600));
@@ -178,6 +185,7 @@ public class ManageRolesForm {
                     fieldGroupName.setText("");
                     scrollPaneUsers.setEnabled(true);
                     comboBoxGroups.setEnabled(false);
+                    listUsers.setEnabled(true);
                 }
             }
         });
@@ -195,6 +203,7 @@ public class ManageRolesForm {
                     comboBoxGroups.setEnabled(true);
                     fieldGroupName.setEnabled(true);
                     scrollPaneUsers.setEnabled(true);
+                    listUsers.setEnabled(true);
                 }
             }
         });
@@ -204,6 +213,9 @@ public class ManageRolesForm {
          * Enables fieldGroupName and scrollPaneUsers
          * Populates fieldGroupName with the group name from DB for selected index
          */
+        //TODO: FUTURE FEATURE 01.b:
+        // See FF 01.a
+        // Select check boxes next to user names in list when group is selected
         comboBoxGroups.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
@@ -211,14 +223,19 @@ public class ManageRolesForm {
                     int selectedIndex = comboBoxGroups.getSelectedIndex();
                     if (selectedIndex == 0) {
                         fieldGroupName.setText("");
+                        fieldGroupDesc.setText("");
                     } else {
+                        //TODO: Get the group Description in addition to GroupName - Will
                         String fieldText = "";
+                        //String fieldDesc = "";
                         for(Integer key : groups.keySet()) {
                             if(selectedIndex == key) {
                                 fieldText = groups.get(key).get(0).toString();
+                                //fieldDesc =
                             }
                         }
                         fieldGroupName.setText(fieldText);
+                        //fieldGroupDesc.setText(fieldDesc);
                     }
                 }
             }
@@ -234,7 +251,10 @@ public class ManageRolesForm {
             @Override
             public void actionPerformed(ActionEvent e) {
                 boolean checkGroupName = false;
+                boolean checkGroupDesc = false;
 
+                // Check that the Group Name field has something in it and
+                // set that text to the variable for DB update
                 if(fieldGroupName.getText().equals("")) {
                     JOptionPane.showMessageDialog(null, "Please enter a Group Name");
                 } else {
@@ -242,14 +262,28 @@ public class ManageRolesForm {
                     checkGroupName = true;
                 }
 
-                if(checkGroupName) {
+                // Check that the Description field has something in it and
+                // set that text to the variable for DB update
+                if(fieldGroupDesc.getText().equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please enter a description for the Group");
+                } else {
+                    newGroupDesc = fieldGroupDesc.getText();
+                    checkGroupDesc = true;
+                }
+
+                // Make sure the group has both fields filled in and
+                // call methods to submit changes to DB
+                if(checkGroupName && checkGroupDesc) {
                     if(radioCreateNew.isSelected()) {
-                        //TODO: Save new Group details to DB
+                        //TODO: FUTURE FEATURE 02: Add functionality to include users in group (different table USER_GROUP)
                         JOptionPane.showMessageDialog(null, "Group Created");
+                        subs.logNewGroup(newGroupName, newGroupDesc);
                     }
                     else if (radioEditExisting.isSelected()) {
-                        //TODO: Update group details in DB
+                        //TODO: FUTURE FEATURE 03: Add functionality to update users in group (different table USER_GROUP)
+                        existingGroupID = comboBoxGroups.getSelectedIndex();
                         JOptionPane.showMessageDialog(null, "Group Updated");
+                        subs.updateExistingGroup(existingGroupID, newGroupName, newGroupDesc);
                     }
                 }
             }
@@ -292,18 +326,17 @@ public class ManageRolesForm {
         comboBoxGroups.setModel(modelGroups);
     }
 
+    /**
+     * Model for the user scroll list to select multiple users
+     */
     private void populateSubscribers() {
-        //TODO: populate multi-select list (scrollPaneUsers) with all users
+        //TODO: Sort list alphabetically
+        //TODO: FUTURE FEATURE 01.a:
+        // Add Check Boxes next to users to indicate if they're in the selected group or not
         this.listUsers = new JList();
-        //TODO: Convert recipients ArrayList to JList
-<<<<<<< HEAD
         //listUsers.setListData(recipients.toArray());
-=======
-//        listUsers.setListData(recipients.toArray());
->>>>>>> origin/synSprint2
         DefaultListModel model = new DefaultListModel();
         model.removeAllElements();
-        model.addElement("All Users");
         for(Recipient recipient : recipients) {
             model.addElement(recipient.getUserName());
         }
