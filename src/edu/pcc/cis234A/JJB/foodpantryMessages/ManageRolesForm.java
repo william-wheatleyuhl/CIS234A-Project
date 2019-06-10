88eaa793301e30e8e4b98ab88d9aad03a2e74cf1
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2019-06-10 02:18:15 -0700

    Implement populateSubscribers method call

--- src/edu/pcc/cis234A/JJB/foodpantryMessages/ManageRolesForm.java
+++ src/edu/pcc/cis234A/JJB/foodpantryMessages/ManageRolesForm.java
@@ -63,6 +63,7 @@ public class ManageRolesForm {
         populateUsers();
         populateRoles();
         populateGroups();
+        populateSubscribers();
         comboBoxRoles.setEnabled(false);
         checkBoxConfirm.setEnabled(false);
         checkBoxConfirm.setVisible(false);
@@ -295,7 +296,7 @@ public class ManageRolesForm {
         //TODO: populate multi-select list (scrollPaneUsers) with all users
         this.listUsers = new JList();
         //TODO: Convert recipients ArrayList to JList
-        listUsers.setListData(recipients.toArray());
+//        listUsers.setListData(recipients.toArray());
         DefaultListModel model = new DefaultListModel();
         model.removeAllElements();
         model.addElement("All Users");
